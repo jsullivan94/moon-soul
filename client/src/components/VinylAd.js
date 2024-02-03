af27e4569d2f93d4aad848ff89611ca3c5175521
james-sullivan <jsullivan8794@gmail.com>
2024-02-03 14:23:09 -0500

    update image paths

--- client/src/components/VinylAd.js
+++ client/src/components/VinylAd.js
@@ -4,7 +4,7 @@ function VinylAd() {
     const navigate = useNavigate()
     return (
         <div className='vinyl-ad-card' onClick={() => navigate('/product/1')}>
-            <img className="vinyl-ad-img" src='/pictures/albumcover.png' alt='vinyl' />
+            <img className="vinyl-ad-img" src='/build/pictures/albumcover.png' alt='vinyl' />
             <h1 className='vinyl-ad-text' >Buy our new vinyl!</h1>
         </div>
     )
