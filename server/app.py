fc353680aa90769bb19159ac82d7662247c4870b
james-sullivan <jsullivan8794@gmail.com>
2024-02-03 12:29:00 -0500

    move config to app

--- server/app.py
+++ server/app.py
@@ -1,14 +1,39 @@
 from flask import request, make_response, jsonify, session, Flask, render_template, send_from_directory
 from sqlalchemy.exc import IntegrityError
+from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 import uuid
 import json
 import os
 import stripe 
 import requests
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_cors import CORS
 
-from config import *
 from models import *
+
+
+app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+CORS(app, supports_credentials=True)
+uri = os.getenv("DATABASE_URL", "sqlite:///app.db")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
 
 load_dotenv()
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
