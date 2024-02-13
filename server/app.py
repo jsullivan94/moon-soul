863b84a88b6e7610df120a1ea835ebb295ece805
james-sullivan <jsullivan8794@gmail.com>
2024-02-13 13:30:16 -0500

    config update

--- server/app.py
+++ server/app.py
@@ -15,10 +15,11 @@ load_dotenv()
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
 
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
