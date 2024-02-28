ec08e7ce98494715a0e732d416bf284dc103290a
james-sullivan <jsullivan8794@gmail.com>
2024-02-27 21:28:41 -0500

    change static_folder path

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 import os
 
 
-app = Flask(__name__, static_folder='../client/build', static_url_path='')
+app = Flask(__name__, static_folder='client/build', static_url_path='')
 app.debug = False
 CORS(app, supports_credentials=True)
 uri = os.getenv("DATABASE_URL", "sqlite:///app.db")  # or other relevant config var
